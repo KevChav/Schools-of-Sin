888d31ccfdfbe17506c403760540fcef1f7de969
KevChav <kevin.superkbc@gmail.com>
2024-05-28 15:23:46 -0500

    Added Templates
    Added templates to create more unique enemies and cards. Used these templates to create a bat enemy and an attack that hits all enemies

---
+++ script_templates/Node/effect_template.gd
@@ -0,0 +1,11 @@
+# mtea-name: Effect
+# meta-description: Create an effect which can be applied to a target
+class_name MyAwesomeEffect
+extends Effect
+
+var member_var := 0
+
+func execute (targets: Array[Node]) -> void:
+	print("My effet targets them: %s" % targets)
+	print("It does %s something" % member_var)
+	
